45d822f0abae88fbcab0ed17d5aec778a7d68e93
Vanja Đenadija <vanja.github@gmail.com>
2023-02-19 23:25:29 +0100

    DB Files rearranged

--- resources/db/partnerska_kompanija.sql
+++
@@ -1,9 +0,0 @@
-/*
--- Query: SELECT * FROM bayensolutions.partnerska_kompanija
-LIMIT 0, 1000
-
--- Date: 2023-01-13 16:49
-*/
-INSERT INTO `` (`IdPartnerskaKompanija`,`Naziv`,`Adresa`,`Email`,`Telefon`) VALUES (1,'Limocentar d.o.o.','Put srpskih branilaca 115, Banja Luka','limocentardoo@gmail.com','051 370-694');
-INSERT INTO `` (`IdPartnerskaKompanija`,`Naziv`,`Adresa`,`Email`,`Telefon`) VALUES (2,'Abi d.o.o.','Ulica generala Izeta Nanića b.b. Novi Grad','abi@gmail.com','065 480-923');
-INSERT INTO `` (`IdPartnerskaKompanija`,`Naziv`,`Adresa`,`Email`,`Telefon`) VALUES (3,'Belif d.o.o.','Poslovna zona Ciglana br. 14 Jelah','info@belif.ba','032 664-555');
--- resources/db/zaposleni.sql
+++
@@ -1,9 +0,0 @@
-/*
--- Query: SELECT * FROM bayensolutions.zaposleni
-LIMIT 0, 1000
-
--- Date: 2023-01-13 16:45
-*/
-INSERT INTO `` (`KorisničkoIme`,`OtisakLozinke`,`Plata`,`JMBG`,`OSOBA_IdOsoba`,`ULOGA_ZAPOSLENOG_IdUloga`) VALUES ('BOBI','bobilica',2000.00,'0105972113243',1,1);
-INSERT INTO `` (`KorisničkoIme`,`OtisakLozinke`,`Plata`,`JMBG`,`OSOBA_IdOsoba`,`ULOGA_ZAPOSLENOG_IdUloga`) VALUES ('bayen','bayen',0.00,'1404998100019',4,3);
-INSERT INTO `` (`KorisničkoIme`,`OtisakLozinke`,`Plata`,`JMBG`,`OSOBA_IdOsoba`,`ULOGA_ZAPOSLENOG_IdUloga`) VALUES ('komso','gak',50.00,'0000000000000',15,2);
